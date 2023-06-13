8496405f9d003b3ebf203801cdb44fbd0965c03a
Samuel Barton <srbarton43@gmail.com>
2023-06-13 10:40:19 -0400

    Fix fzf to ignore .git directory

--- explorer.sh
+++ explorer.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
 # Store the STDOUT of fzf in a variable
-selection=$(find . -type d -not -path '*/\.git' | fzf-tmux -p --multi --height=80% --border=sharp \
---preview='tree -C {}' --preview-window='45%,border-sharp' \
---prompt='Dirs > ' \
---bind='del:execute(rm -ri {+})' \
---bind='ctrl-o:toggle-preview' \
---bind='ctrl-d:change-prompt(Dirs > )' \
---bind='ctrl-d:+reload(find . -type d -not -path "*/\.git")' \
---bind='ctrl-d:+change-preview(tree -C {})' \
---bind='ctrl-d:+refresh-preview' \
---bind='ctrl-f:change-prompt(Files > )' \
---bind='ctrl-f:+reload(find . -type f -not -path "*/\.git/*")' \
---bind='ctrl-f:+change-preview(cat {})' \
---bind='ctrl-f:+refresh-preview' \
---bind='ctrl-a:select-all' \
---bind='ctrl-x:deselect-all' \
---pointer='󰜴' --marker='󰄳 ' --multi --color='dark,fg+:red,hl:cyan,hl+:yellow,label:yellow,info:grey' \
---border-label="Fuzzy File Explorer"
+selection=$(find . -type d -not -path '*/\.git/*' -and -not -path './.git' \
+  | fzf --multi --height=80% --border=sharp \
+  --preview='tree -C {}' --preview-window='45%,border-sharp' \
+  --prompt='Dirs > ' \
+  --bind='del:execute(rm -ri {+})' \
+  --bind='ctrl-o:toggle-preview' \
+  --bind='ctrl-d:change-prompt(Dirs > )' \
+  --bind='ctrl-d:+reload(find . -type d -not -path "*/\.git")' \
+  --bind='ctrl-d:+change-preview(tree -C {})' \
+  --bind='ctrl-d:+refresh-preview' \
+  --bind='ctrl-f:change-prompt(Files > )' \
+  --bind='ctrl-f:+reload(find . -type f -not -path "*/\.git/*")' \
+  --bind='ctrl-f:+change-preview(cat {})' \
+  --bind='ctrl-f:+refresh-preview' \
+  --bind='ctrl-a:select-all' \
+  --bind='ctrl-x:deselect-all' \
+  --pointer='󰜴' --marker='󰄳 ' --multi --color='dark,fg+:red,hl:cyan,hl+:yellow,label:yellow,info:grey' \
+  --border-label="Fuzzy File Explorer"
 )
 
 # Determine what to do depending on the selection
