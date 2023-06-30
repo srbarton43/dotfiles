36fe185025509f5f1ae7743fbf48ed37baf9f5fb
Samuel Barton <srbarton43@gmail.com>
2023-06-29 21:02:46 -0400

    Fix scrolling through cmp docs which were reversed

--- config/nvim/lua/config/nvim-cmp.lua
+++ config/nvim/lua/config/nvim-cmp.lua
@@ -29,8 +29,8 @@ cmp.setup {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ["<C-e>"] = cmp.mapping.abort(),
     ["<Esc>"] = cmp.mapping.close(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-u>"] = cmp.mapping.scroll_docs(4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
 
     ['<C-f>'] = cmp.mapping(function(fallback)
       if luasnip.jumpable(1) then
