b78503ea37f6ea1bf3d5c95c91e0184de7a658ca
Samuel Barton <srbarton43@gmail.com>
2023-07-20 12:36:57 -0400

    Fix cd-widget namespace bug

--- zsh/funcs.zsh
+++ zsh/funcs.zsh
@@ -33,40 +33,32 @@ killp() {
   fi
 }
 
-# array of bad directorys DONT SEARCH
-local DIRS=('.git' "Music" "packer" "release" "target" "python3.11" "node_modules"
-      "Photos Library.photoslibrary" "cache" "build" "Application Support"
-      "Data" "Packages" ".cargo" ".vscode" "Applications" "Library" "tmp" 
-      ".rustup" '.docker')
-#construct 'find' flags string
-local str="find . -type d"
-for ((i=1; i<${#dirs[@]}; i++)); do
-  str="$str -not \( -path '"*${DIRS[i]}*"' -prune \) "
-done
-str="$str -not \( -path '.' \)"
 
 fzf-cd-widget() {
+  # array of bad directorys DONT SEARCH
+  local DIRS=(".git" "Music" "packer" "release" "target" "python3.11" "node_modules"
+        "Photos Library.photoslibrary" "cache" "build" "Application Support"
+        "Data" "Packages" ".cargo" ".vscode" "Applications" "Library" "tmp" 
+        ".rustup" ".docker")
+  #construct 'find' flags string
+  local str="find . -type d"
+  for ((i=1; i<${#DIRS[@]}+1; i++)); do
+    str="$str -not \( -path '"*${DIRS[i]}*"' -prune \) "
+  done
+  str="$str -not \( -path '.' \)"
   selection=$(eval "$str" \
-      | fzf --multi --height=80% --border=sharp \
+      | fzf --height=80% --border=sharp \
       --preview='tree -C {}' --preview-window='45%,border-sharp' \
       --prompt='Dirs > ' \
       --bind='ctrl-o:toggle-preview' \
-      --bind='ctrl-d:change-prompt(Dirs > )' \
-      --bind='ctrl-d:+reload(find . -type d -not -path "*/\.git")' \
-      --bind='ctrl-d:+change-preview(tree -C {})' \
-      --bind='ctrl-d:+refresh-preview' \
-      --bind='ctrl-f:change-prompt(Files > )' \
-      --bind='ctrl-f:+reload(find . -type f -not -path "*/\.git/*")' \
-      --bind='ctrl-f:+change-preview(cat {})' \
-      --bind='ctrl-f:+refresh-preview' \
       --bind='ctrl-a:select-all' \
       --bind='ctrl-x:deselect-all' \
       --pointer='󰜴' --marker='󰄳 ' --multi --color='dark,fg+:red,hl:cyan,hl+:yellow,label:yellow,info:grey' \
       --border-label="Fuzzy File Explorer"
     )
-    cd "$selection"
+  cd "$selection"
   zle reset-prompt
+  unset str
+  unset DIRS
   return 0
 }
-unset str
-unset DIRS
