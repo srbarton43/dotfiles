1ed0d4547d85558ad7d0f53b5e6e54d7415325ec
Samuel Barton <srbarton43@gmail.com>
2023-08-08 13:07:20 -0400

    Add buffers telescope plugin as <leader>fb

--- config/nvim/lua/config/telescope-conf.lua
+++ config/nvim/lua/config/telescope-conf.lua
@@ -12,3 +12,4 @@ local opts = {
 vim.keymap.set('n', '<leader>ff', ':lua require("telescope.builtin").find_files{}<CR>', opts)
 vim.keymap.set('n', '<leader>fr', ':lua require("telescope.builtin").oldfiles{}<CR>', opts)
 vim.keymap.set('n', '<leader>fg', ':lua require("telescope.builtin").live_grep{}<CR>', opts)
+vim.keymap.set('n', '<leader>fb', ':lua require("telescope.builtin").buffers{}<CR>', opts)
