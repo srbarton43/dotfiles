c16240e306e846ebe9d952fd89fa2ca52708c273
Samuel Barton <srbarton43@gmail.com>
2023-11-17 15:17:40 -0500

    Add keybinds for fzf plugin widgets

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -7,3 +7,13 @@ zle -N fzf-paste-widget
 bindkey -M emacs '\C-p' fzf-paste-widget
 bindkey -M vicmd '\C-p' fzf-paste-widget
 bindkey -M viins '\C-p' fzf-paste-widget
+
+zle -N fzf-paste-widget-cwd
+bindkey -M emacs '\C-o' fzf-paste-widget-cwd
+bindkey -M vicmd '\C-o' fzf-paste-widget-cwd
+bindkey -M viins '\C-o' fzf-paste-widget-cwd
+
+zle     -N            fzf-history-widget
+bindkey -M emacs '\C-r' fzf-history-widget
+bindkey -M vicmd '\C-r' fzf-history-widget
+bindkey -M viins '\C-r' fzf-history-widget
