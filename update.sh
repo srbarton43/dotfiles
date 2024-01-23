fd9627474a4409592606437c4a4eb32a134cf5e0
Samuel Barton <srbarton43@gmail.com>
2024-01-22 20:49:43 -0500

    remove sketchy install script

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "Updating dotfiles in home directory from $SCRIPT_DIR"
-
-for FILE in gitconfig tmux.conf viminfo vimrc zshrc config vim zsh
-do
-	ln -shF ${SCRIPT_DIR}/${FILE} ~/.${FILE}
-done
