c1c42595204c873aaa8c4f07b64aa5b91c3bddfa
Samuel Barton <srbarton43@gmail.com>
2024-02-05 13:44:57 -0500

    Add changes to fish env vars and abbr's

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,17 +14,33 @@ if status is-interactive
   # visual mode, but due to fish_cursor_default, is redundant here
   set fish_cursor_visual block
 
-  # keybindings
+  ## keybindings
   bind \ce 'fzf_cd_widget'
   bind \cp 'fzf_paste_widget'
   bind \co 'fzf_paste_cwd_widget'
 
-  # abbreviations
+  ## abbreviations
+  abbr -a -- lsa 'ls -A'
+  abbr -a -- l 'ls -lh'
+  abbr -a -- '-' 'cd -'
+  abbr -a -- cl 'clear'
+  abbr -a -- '...' 'cd ../../'
+  abbr -a -- python python3
+  abbr -a -- pip pip3
+  abbr -a -- oskim 'open -a Skim'
+  abbr -a -- ez 'exec fish'
 
   # git
   abbr -a -- gg 'git status'
   abbr -a -- gcm 'git commit -v'
+  abbr -a -- ga 'git add'
   abbr -a -- gall 'git add .'
+  abbr -a -- gp 'git push'
+  abbr -a -- gl 'git log --graph --one-line'
+
+  ## export variables
+  set -xg LSCOLORS 'gxcxcxdxFxaDadabagacad'
+  set -xg LS_COLORS "di=36:ln=32:so=32:pi=33:ex=1;35:bd=30;1;43:cd=30;43:su=30;41:sg=30;46:tw=30;42:ow=30;43" 
   
   # initialize the prompt
   starship init fish | source
