9e8421e59f3424f3d53cda1a10c2a600c4824f07
Samuel Barton <srbarton43@gmail.com>
2024-02-11 12:56:47 -0500

    Add symlinks to fzf find

--- .config/fish/functions/fzf_cd_cwd_widget.fish
+++ .config/fish/functions/fzf_cd_cwd_widget.fish
@@ -1,5 +1,5 @@
 function fzf_cd_cwd_widget
-  set -l selection (fd . -u -t d --ignore-file ~/.fdignore \
+  set -l selection (fd . -u -t d -t l --ignore-file ~/.fdignore \
     | fzf --height=80% --border=sharp \
     --preview='tree -C {}' --preview-window='45%,border-sharp' \
     --prompt='Dirs > ' \
@@ -9,11 +9,14 @@ function fzf_cd_cwd_widget
     --pointer='󰜴' --marker='󰄳 ' --multi --color='dark,fg+:red,hl:cyan,hl+:yellow,label:yellow,info:grey' \
     --border-label="Fuzzy CD Widget")
   if not test -z $selection
-    echo $selection
-    cd "$selection" 
-    echo "---------------------------------------------"
-    ls -lG
-    echo "---------------------------------------------"
+    if cd "$selection" 2&> /dev/null
+      echo $selection
+      echo "---------------------------------------------"
+      ls -lG
+      echo "---------------------------------------------"
+    else
+      echo "Error: $selection is not a directory" 
+    end
   end
   commandline -f repaint
   return 0
