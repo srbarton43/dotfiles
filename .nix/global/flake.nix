20aef6877256ba9e6f2c73b5319d3b07d14a56db
Samuel Barton <srbarton43@gmail.com>
2024-02-11 12:58:50 -0500

    Start tracking global nix packages

---
+++ .nix/global/flake.nix
@@ -0,0 +1,46 @@
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = {self, nixpkgs}: {
+    packages."aarch64-darwin".default = let
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    in pkgs.buildEnv {
+      name = "home-packages";
+      paths = with pkgs; [
+        pandoc
+        tmux
+        fish
+        meson
+        bash
+        neovim
+        fzf
+        fd
+        bat
+        starship
+        ripgrep
+        llvm
+        bear
+        nixd
+        delta
+        ffmpeg
+        ncurses
+        ninja
+        bear
+        bison
+        wget
+        darwin.trash
+        llvm
+        git-crypt
+        cmake
+        pass
+        fastfetch
+        du-dust
+        bottom
+        htop
+      ];
+    };
+  };
+}
