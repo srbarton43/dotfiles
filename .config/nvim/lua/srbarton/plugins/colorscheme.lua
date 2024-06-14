b59340cf9c4d23339d35819ab72f159bd828f040
Samuel Barton <srbarton43@gmail.com>
2024-06-13 22:09:00 -0400

    Fix colorscheme post neovim 0.10.0 update

--- .config/nvim/lua/srbarton/plugins/colorscheme.lua
+++ .config/nvim/lua/srbarton/plugins/colorscheme.lua
@@ -1,12 +1,40 @@
 return {
-	"tanvirtin/monokai.nvim", -- monokai themes
-	priority = 1000,
-	config = function()
-		require('monokai').setup({
-			palette = {
-				base2 = '#121212', -- make bg darker
-				green = '#86fa04', -- i like this green better
-			},
-		})
-	end,
+  "tanvirtin/monokai.nvim", -- monokai themes
+  priority = 1000,
+  config = function()
+    local monokai = require('monokai')
+    local palette = monokai.classic
+    local nvim_set_hl = vim.api.nvim_set_hl
+    monokai.setup({
+      palette = {
+        base2 = '#121212', -- make bg darker
+        green = '#86fa04', -- i like this green better
+      },
+      custom_hlgroups = {
+        GitSignsAdd = {
+          fg = '#86fa04',   -- green
+        },
+        GitSignsDelete = {
+          fg = palette.pink,
+        },
+        DiagnosticError = {
+          fg = "Red",
+        },
+        DiagnosticWarn = {
+          fg = "Orange",
+        },
+        DiagnosticInfo = {
+          fg = "LightBlue",
+        },
+        DiagnosticHint = {
+          fg = "LightGrey",
+        },
+        DiagnosticOk = {
+          fg = "LightGreen",
+        },
+      },
+    })
+    -- keeps bottom diff intact
+    nvim_set_hl(0, "GitSignsChange", { link = "DiffChange" })
+  end,
 }
