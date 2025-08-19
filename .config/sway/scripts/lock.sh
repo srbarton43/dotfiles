0edb9c6afbb1ab61a644139c79eb64f474df6fa2
Samuel Barton <srbarton43@gmail.com>
2025-08-18 21:15:11 -0400

    Modify sway config

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -6,4 +6,4 @@
 #  --show-failed-attempts \
 #  --clock --indicator
 
-swaylock --ignore-empty-password --show-failed-attempts --image /usr/share/backgrounds/cosmic/orion_nebula_nasa_heic0601a.jpg  --scaling fill --clock --indicator 
+swaylock --ignore-empty-password --show-failed-attempts --image /home/srbarton/.current_background.png  --scaling fill --clock --indicator 
